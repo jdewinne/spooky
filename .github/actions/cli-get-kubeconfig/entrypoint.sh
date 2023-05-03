cf7b32d29f8fae7ed118af2a80ab0df331b7a995
Josh De Winne <josh@dewinne.dev>
2023-05-03 14:51:32 -0700

    test

--- .github/actions/cli-get-kubeconfig/entrypoint.sh
+++ .github/actions/cli-get-kubeconfig/entrypoint.sh
@@ -2,7 +2,6 @@
 
 /replicated cluster kubeconfig --id $CLUSTER_ID
 export KUBECONFIG="$(cat ~/.kube/config)"
-KUBECONFIG="${KUBECONFIG//'%'/'%25'}"
-KUBECONFIG="${KUBECONFIG//$'\n'/'%0A'}"
-KUBECONFIG="${KUBECONFIG//$'\r'/'%0D'}"
-echo "kubeconfig=$KUBECONFIG" >> $GITHUB_OUTPUT
+echo "kubeconfig<<EOF" >> $GITHUB_OUTPUT
+echo "$KUBECONFIG" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
