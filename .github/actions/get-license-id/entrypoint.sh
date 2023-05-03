e59dd1f1b2055ae8393fbca8400ee485dc6eb275
Josh De Winne <josh@dewinne.dev>
2023-05-02 20:45:52 -0700

    test

--- .github/actions/get-license-id/entrypoint.sh
+++ .github/actions/get-license-id/entrypoint.sh
@@ -2,5 +2,10 @@
 
 /replicated $@ > license.json
 
+cat license.json
 LICENSE_ID=$(cat license.json | jq -r '.spec.licenseID')
+echo "license-id=$LICENSE_ID"
+
 echo "license-id=$LICENSE_ID" >> $GITHUB_OUTPUT
+
+cat $GITHUB_OUTPUT
