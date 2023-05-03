ff626ca60881c110b2a90a7d13f1e0f26fbb8a1b
Josh De Winne <josh@dewinne.dev>
2023-05-03 10:34:28 -0700

    test

--- .github/actions/wait-for-cluster/entrypoint.sh
+++ .github/actions/wait-for-cluster/entrypoint.sh
@@ -5,14 +5,14 @@ do
     /replicated cluster ls --output=json > clusters.json
 
     # Does cluster exist?
-    IS_EMPTY=$(cat clusters.json | jq -r 'isempty(.[] | select(.id=="$CLUSTER_ID"))')
+    IS_EMPTY=$(cat clusters.json | jq --arg clusterid "$CLUSTER_ID" 'isempty(.[] | select(.id==$clusterid))')
     if [ "$IS_EMPTY" = "true" ]; then
         echo "Cluster with id $CLUSTER_ID does not exist"
         exit 1
     fi
 
     # Is cluster ready?
-    CLUSTER_STATUS=$(cat clusters.json | jq -r '.[] | select(.id=="$CLUSTER_ID") | .status')
+    CLUSTER_STATUS=$(cat clusters.json | jq -r --arg clusterid "$CLUSTER_ID" '.[] | select(.id==$clusterid) | .status')
     if [ "$CLUSTER_STATUS" = "running" ]; then
         echo "Cluster with id $CLUSTER_ID is running"
         echo "cluster-status=$CLUSTER_STATUS" >> $GITHUB_OUTPUT
