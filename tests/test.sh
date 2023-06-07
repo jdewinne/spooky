16af30988e56b7558bf2521e6114e78489bf368f
Josh De Winne <josh@dewinne.dev>
2023-06-07 19:33:33 -0400

    WIP: smoke test composite action

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Running a test"
 sleep 60
-kubectl port-forward svc/relmatrix-app --pod-running-timeout=2m 8080:80 &
+kubectl port-forward svc/tmp-relmatrix-relmatrix-app --pod-running-timeout=2m 8080:80 &
 sleep 120
 curl -f http://localhost:8080
 echo "Test complete"
